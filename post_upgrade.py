d90de885dd33e4413f37e3fefadeb779acff7638
wheelercj <chrisw289@gmail.com>
2025-09-26 00:21:59 -0700

    Move update of sources.list files to post_upgrade

--- post_upgrade.py
+++ post_upgrade.py
@@ -1,4 +1,74 @@
+import argparse
+import re
+import sys
+from datetime import datetime
+from pathlib import Path
+
+from utils import get_current_version_name
+
+
+now = datetime.now()
+today: str = f"{now.year}-{str(now.month).zfill(2)}-{str(now.day).zfill(2)}"
+
+
 def main():
+    parser = argparse.ArgumentParser(
+        prog="post-upgrade",
+        description="Scripts to help with upgrading to a different major version of Ubuntu",
+    )
+    parser.add_argument("prev_version_name", type=str)
+
+    args = parser.parse_args()
+    prev_version_name: str = args.prev_version_name
+    if " " in prev_version_name:
+        print('Error: enter only the first part of the new version\'s name, such as "jammy"')
+        sys.exit(1)
+
+    version_name: str = get_current_version_name()
+    if version_name == prev_version_name:
+        print("Error: enter the name of the version you upgraded from, not the current version")
+        sys.exit(1)
+
+    print("Getting source repositories")
+    srcs_list: Path = Path("/etc/apt/sources.list")
+    srcs_list_d: Path = Path("/etc/apt/sources.list.d/")
+
+    print("Updating sources files")
+    update_sources_file(srcs_list, version_name, prev_version_name)
+    for path in srcs_list_d.iterdir():
+        if path.is_file():
+            update_sources_file(path, version_name, prev_version_name)
+
+
+def update_sources_file(file: Path, version_name: str, prev_version_name: str) -> None:
+    """Changes a sources file from an old version of Ubuntu to the current version"""
+    contents: str = file.read_text(encoding="utf8")
+
+    # replace the old version name with the current version name
+    new_contents: str = re.sub(
+        pattern=rf"^(deb.+){prev_version_name}(.+)",
+        repl=rf"#\1{prev_version_name}\2\n\1{version_name}\2",
+        string=contents,
+        flags=re.MULTILINE,
+    )
+    if contents == new_contents:
+        print(f"Skipping file that has no {prev_version_name} repository definitions: {file}")
+        return
+
+    # make a backup of the file
+    backups_folder: Path = Path(file.parent / "backups")
+    backups_folder.mkdir(exist_ok=True)
+    backup: Path = backups_folder / f"{today}_{file.name}.bak"
+    backup.write_text(contents, encoding="utf8")
+    print(f"Backup of {file.name} created at {backup}")
+
+    # create a temporary file with the new contents
+    temp: Path = Path(f"{file}.tmp")
+    temp.write_text(new_contents, encoding="utf8")
+
+    # rename the temporary file to replace the original file
+    temp.replace(file)
+    print(f"Updated {file}")
 
 
 if __name__ == "__main__":
--- pre_upgrade.py
+++ pre_upgrade.py
@@ -14,7 +14,7 @@ today: str = f"{now.year}-{str(now.month).zfill(2)}-{str(now.day).zfill(2)}"
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="upgrade-ubuntu",
+        prog="pre-upgrade",
         description="Scripts to help with upgrading to a different major version of Ubuntu",
     )
     parser.add_argument("new_version_name", type=str)
@@ -30,8 +30,6 @@ def main():
         print("Error: enter the name of the version to change to, not the current version")
         sys.exit(1)
 
-    print(f"Upgrading from {version_name} to {new_version_name}")
-
     print("Getting a list of the manually installed packages")
     showmanual_res: subprocess.CompletedProcess = subprocess.run(
         ["apt-mark showmanual"],
@@ -43,16 +41,6 @@ def main():
     assert isinstance(showmanual_res.stdout, str), f"{type(showmanual_res.stdout).__name__ = }"
     manually_installed: list[str] = showmanual_res.stdout.strip().splitlines()
     print(f"Found {len(manually_installed)} manually installed packages")
-
-    print("Getting source repositories")
-    srcs_list: Path = Path("/etc/apt/sources.list")
-    srcs_list_d: Path = Path("/etc/apt/sources.list.d/")
-
-    print("Updating sources files")
-    update_sources_file(srcs_list, version_name, new_version_name)
-    for path in srcs_list_d.iterdir():
-        if path.is_file():
-            update_sources_file(path, version_name, new_version_name)
 
     print("Indexing /var/lib/apt/lists")
     apt_lists_pkg_urls: dict[str, str] = index_apt_lists()
@@ -69,36 +57,6 @@ def main():
     print()
     for pkg, url in chosen_pkg_urls.items():
         print(f"{pkg}:\n\t{url}")
-
-
-def update_sources_file(file: Path, version_name: str, new_version_name: str) -> None:
-    """Changes a sources file to use a new version of Ubuntu"""
-    contents: str = file.read_text(encoding="utf8")
-
-    # replace the current version name with the new version name
-    new_contents: str = re.sub(
-        pattern=rf"^(deb.+){version_name}(.+)",
-        repl=rf"#\1{version_name}\2\n\1{new_version_name}\2",
-        string=contents,
-        flags=re.MULTILINE,
-    )
-    if contents == new_contents:
-        print(f"Skipping file that has no {version_name} repository definitions: {file}")
-        return
-
-    # make a backup of the file
-    backups_folder: Path = Path(file.parent / "backups")
-    backups_folder.mkdir(exist_ok=True)
-    backup: Path = backups_folder / f"{today}_{file.name}.bak"
-    backup.write_text(contents, encoding="utf8")
-    print(f"Backup of {file.name} created at {backup}")
-
-    # create a temporary file with the new contents
-    temp: Path = Path(f"{file}.tmp")
-    temp.write_text(new_contents, encoding="utf8")
-
-    # rename the temporary file to replace the original file
-    temp.replace(file)
 
 
 def index_apt_lists() -> dict[str, str]:
